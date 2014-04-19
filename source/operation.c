1988c6134daa1df001526042f41755bd8859dadf
buxiaoyang <buxiaoyang@gmail.com>
2014-04-19 22:51:03 +0800

    恢复“添加出错信号，取消出错保护，一旦过冲就提示出错”版本

--- source/operation.c
+++ source/operation.c
@@ -5,7 +5,6 @@
 #include <operation.h>
 #include <intrins.h>
 
-//输出
 sbit MotorLeft = P2^0; //机械臂向进口移动电机
 sbit MotorRight = P2^1; //机械臂向出口移动电机
 sbit ManiUp =  P1^0; //机械臂向上移动电机
@@ -13,12 +12,12 @@ sbit ManiDown =  P1^1; //机械臂向下移动电机
 sbit MotorSpeedUp =  P1^2; //辅助输出（电机加速）
 sbit MotorSlowDown = P1^3; //辅助输出（电机减速）
 
-//输入
-sbit sensorManiTop = P1^4; //机械臂在顶端位置感应
-sbit sensorManiBut = P1^7; //机械臂在低端位置感应
+sbit SystemError = P1^4; //系统出错指示灯
 
-sbit sensorPositionInput = P3^0; //进口位置感应
-sbit sensorPositionOutput = P3^7; //出口位置感应
+sbit sensorInput = P3^0; //进口位置感应
+sbit sensorManiTop = P3^1; //机械臂在顶端位置感应
+sbit sensorManiBut = P3^2; //机械臂在低端位置感应
+sbit sensorOutput = P3^3; //出位置感应
 
 
 void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber); //机械臂提起upCisternNumber槽里面框放到downCisternNumber槽
@@ -48,7 +47,8 @@ void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber)
 	{
 		case 0: 	//0：机械臂初始状态
 			ManiOperationTimer = 0;
-			A_Up_Down_Steps ++;
+			SystemError = 1;
+			A_Up_Down_Steps ++;	
 		break;
 		case 1: 	//1：机械臂A定位前等待
 			TargetCisternNumber =  upCisternNumber;	//设置本次机械臂运行的目标槽位
@@ -136,6 +136,7 @@ void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber)
 		case 3: 	//1：机械臂A定位前等待（过冲保护）
 			if(ManiOperationTimer > timeBetweenOpeartion) //定时时间到
 			{
+				/*
 				if(manipulator.currentPosition > upCisternNumber) //当前机械臂在cisternNumber号槽右边
 				{
 					MotorLeft = 0;	//行车向左 
@@ -164,8 +165,14 @@ void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber)
 					}
 					A_Up_Down_Steps = 2;
 				}
+				*/
+				if(manipulator.currentPosition != upCisternNumber)
+				{
+					SystemError = 0;
+				}
 				else
 				{
+					SystemError = 1;
 					MotorLeft = 1;
 					MotorRight = 1;
 					MotorSpeedUp = 1;
@@ -288,6 +295,7 @@ void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber)
 		case 9: 	//1：机械臂A定位前等待（过冲保护）
 			if(ManiOperationTimer > timeBetweenOpeartion) //定时时间到
 			{
+				/*
 				if(manipulator.currentPosition > downCisternNumber) //当前机械臂在cisternNumber号槽右边
 				{
 					MotorLeft = 0; //行车向左 
@@ -316,8 +324,14 @@ void A_Up_Down(uchar upCisternNumber, uchar downCisternNumber)
 					}
 					A_Up_Down_Steps = 8;
 				}
+				*/
+				if(manipulator.currentPosition != downCisternNumber)
+				{
+					SystemError = 0;
+				}
 				else
 				{
+					SystemError = 1;
 					MotorLeft = 1;
 					MotorRight = 1;
 					MotorSpeedUp = 1;
@@ -378,7 +392,7 @@ void A_Move_Entrance(void)
 			}
 		break;
 		case 2: 	//2：机械臂A定位
-			if(sensorPositionInput == 0)
+			if(sensorInput == 0)
 			{
 				ManiOperationTimer = 0;
 				MotorLeft = 1;
--- source/position.c
+++ source/position.c
@@ -3,82 +3,47 @@
 #include <parameter.h>
 #include <basefunc.h>
 
-//sbit sensorPositon = P3^4; //机械臂定位传感器
-//机械臂定位传感器
-#define sensorPositon P3
-
+sbit sensorPositon = P3^4; //机械臂定位传感器
 
 uchar sensorPosiCheck = 0; //机械臂A位置检测标志位 
 							//0：没有感应器没有低电位状态
 							//1：检测到低电位进入延迟状态
 							//2：等待低电位释放进入延时
-							//3：检测低电位确实释放 
-
-uchar getPositonBySensor() //根据传感器获取机械臂位置
-{
-	if(sensorPositon == 0xFE) 	//进口
-	{
-		return 	0;
-	}
-	else if(sensorPositon == 0xFD) //一号槽
-	{
-		return 	2;
-	}
-	else if(sensorPositon == 0xFB)	 //二号槽
-	{
-		return 	4;
-	}
-	else if(sensorPositon == 0xF7)	//三号槽
-	{
-		return 	6;
-	}
-	else if(sensorPositon == 0xEF)	 //四号槽
-	{
-		return 	8;
-	}
-	else if(sensorPositon == 0xDF)	  //五号槽
-	{
-		return 	10;
-	}
-	else if(sensorPositon == 0xBF)	 //六号槽
-	{
-		return 	12;
-	}
-	else if(sensorPositon == 0x7F)	//出口
-	{
-		return 	14;
-	}
-	else
-	{
-		//传感器错误
-		runMode = ManualMode;
-		return 100;
-	}	
-}
+							//3：检测低电位确实释放
 
 void maniPosition(void) //机械臂定位
 {
 	//机械臂A位置检测
 	if(sensorPosiCheck==0)
 	{
-		if(sensorPositon != 0xFF)
+		if(sensorPositon == 0)
 		{
 			sensorPosiCheck = 1;
 		}
 	}
 	else if(sensorPosiCheck==1)
 	{
-		if(sensorPositon != 0xFF)
+		if(sensorPositon == 0)
 		{
 			if(drivingDirect == DGoOutfall)
 			{
 				//向右机械臂位置加一
-				manipulator.currentPosition = getPositonBySensor();
+				manipulator.currentPosition++;
+				displayFlag = 1;
+				if(manipulator.currentPosition>14)
+				{
+					manipulator.currentPosition = 14;
+				}
 			}
 			else if (drivingDirect == DGoEntrance)
 			{
 				//向右机械臂位置减一
-				manipulator.currentPosition = getPositonBySensor();	
+				manipulator.currentPosition--;
+				displayFlag = 1;
+				if(manipulator.currentPosition>200)
+				{
+					manipulator.currentPosition = 0;
+				}	
 			}
 			sensorPosiCheck = 2;
 		}
@@ -90,14 +55,14 @@ void maniPosition(void) //机械臂定位
 	}
 	else if(sensorPosiCheck==2)
 	{
-		if(sensorPositon == 0xFF)
+		if(sensorPositon == 1)
 		{
 			sensorPosiCheck = 3;
 		}	
 	}
 	else
 	{
-		if(sensorPositon == 0xFF)
+		if(sensorPositon == 1)
 		{
 			if(drivingDirect == DGoOutfall)
 			{
